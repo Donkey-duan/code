72c3ca0b342ebcc846250a0795fbb57f240d0ac2
Donkey-duan <jakoo67884520@gmail.com>
2021-10-11 16:49:26 +0800

    master

--- csolusion/chapter3/3.38.c
+++ csolusion/chapter3/3.38.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int num, counter = 0, i;
+    int num, counter = 0, i, dig[5];
     printf("Enter a number(5 digits or fewer): ");
     scanf("%d", &num);
-    for(i = 1; i <= num; i++)
-{
-    if(i % 10000 % 1000 % 100 % 10 == 9)
+    int flag = num;
+    for(i = 0; i <= 4; i++)
     {
-        counter++;
+        dig[i] = flag % 10;
+        flag /= 10;
+        if(dig[i] == 9)
+        {
+            counter++;
+        }
     }
-}
     printf("%d have %d number of 9s.\n", num, counter);
     return 0;
 }
