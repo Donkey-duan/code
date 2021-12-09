ed3f2f7595f41da921341e1533f9c899e1c80a3f
Donkey-duan <jakoo67884520@gmail.com>
2021-12-09 11:16:14 +0800

    master

--- csolusion/考古題/圈圈叉叉.c
+++ csolusion/考古題/圈圈叉叉.c
@@ -1,6 +1,33 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {//還在想
-    
+    char ox[3][4];
+    int counter = 0;
+    scanf("%4s%4s%4s", ox[0],ox[1],ox[2]);
+    for(int i = 0;i<3;i++)
+    {
+        if((ox[i][0]==ox[i][1]) && (ox[i][0]==ox[i][2]))
+        {
+            printf("%c win the game\n", ox[i][0]);
+            counter++;
+            break;
+        }
+        else if((ox[0][i]==ox[1][i]) && (ox[0][i]==ox[2][i]))
+        {
+           printf("%c win the game\n", ox[0][i]); 
+           counter++;
+            break;
+        }
+    }
+    if((ox[0][0]==ox[1][1] && ox[0][0]==ox[2][2]) || (ox[2][0]==ox[1][1] && ox[2][0]==ox[0][2]))
+    {
+        printf("%c win the game\n", ox[1][1]); 
+        counter++;
+    }
+    if(counter==0)
+    {
+        printf("Even\n");
+    }
     return 0;
 }
--- csolusion/考古題/計算上下界中0-9各出現幾次.c
+++ csolusion/考古題/計算上下界中0-9各出現幾次.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 int main()
 {//還沒好
-    int down,up;
-    int count[10]={0};
-    scanf("%d%d", &down,&up);
-    
-    for(int i = 0;i<=9;i++)
-    {
-        printf("%d有%d個\n",i,count[i]);
-    }
+    char a;
+    char all[10];
+    scanf("%c", &a);
+    all = a;
+    printf("%s", all);
     return 0;
 }
