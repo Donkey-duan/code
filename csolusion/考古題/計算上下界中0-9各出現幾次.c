ea74fdd513c22bf39d12d35c418b1c82bcf86a68
Donkey-duan <jakoo67884520@gmail.com>
2021-12-09 11:25:28 +0800

    master

--- csolusion/考古題/計算上下界中0-9各出現幾次.c
+++ csolusion/考古題/計算上下界中0-9各出現幾次.c
@@ -1,10 +1,24 @@
 #include<stdio.h>
 int main()
 {//還沒好
-    char a;
-    char all[10];
-    scanf("%c", &a);
-    all = a;
-    printf("%s", all);
+    int num1, num2;
+    int a[20]={0};
+    scanf("%d %d", &num1, &num2);
+    for(;num1<=num2;num1++)
+    {
+        if(num1<10)
+        {
+            a[num1%10]++;
+        }
+        else
+        {
+            a[num1/10]++;
+            a[num1%10]++;
+        }
+    }
+    for(int i = 0;i<10;i++)
+    {
+        printf("%d有%d個\n", i, a[i]);
+    }
     return 0;
 }
