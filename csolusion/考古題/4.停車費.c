72ca7242be0b0ba333b0f2fc08c490c7c7a7145f
Donkey-duan <jakoo67884520@gmail.com>
2022-01-06 22:17:44 +0800

    master

--- csolusion/考古題/4.停車費.c
+++ csolusion/考古題/4.停車費.c
@@ -1,9 +1,49 @@
 #include<stdio.h>
 int money(int);
+int sum;
 int main()
 {
-    //求正確題意，同學轉述不清楚
+    int min;
+    while(1)
+    {
+        sum = 0;
+        scanf("%d",&min);
+        if(min == 0)
+        {
+            break;
+        }
+        int hr = min/60;
+        if(min%60!=0)
+        {
+            printf("%d\n",money(hr)+60);
+        }
+        else
+        {
+            printf("%d\n",money(hr));
+        }
+    }
     return 0;
+}
+int money(int x)
+{
+    if(x<2)
+    {
+        sum+=60;
+        return sum;
+    }
+    if(x>=2 && x<5)
+    {
+        sum+=40;
+        x--;
+        return money(x);
+    }
+    else
+    {
+        sum+=20;
+        x--;
+        return money(x);
+    }
+    
 }
 /************************************
  * 題目：                            
@@ -12,4 +52,5 @@ int main()
  * 2hr<x<5hr=40元/hr
  * x>5hr=20元/hr
  * 除不盡的剩餘分鐘數就跑到<2hr那邊
+ * 齁跑跑看就知道了好難講
  * **********************************/
