152fb3a93b0e67c5275a6935855662a9d10b17d7
Donkey-duan <jakoo67884520@gmail.com>
2022-05-19 15:44:11 +0800

    master

--- csolution/class/一下/05122.cpp
+++ csolution/class/一下/05122.cpp
@@ -1,5 +1,35 @@
-#include<iostream>
-using namespace std;
-int main(){
-    
-}
+#include<iostream>  
+#include<string>  
+#include<algorithm>  
+using namespace std;  
+int cmp(int a,int b){  
+    return a > b;  
+}  
+int main(){  
+    int count;  
+    cin>>count>>ws;  
+    for(int i = 0;i<count;i++){  
+        string s,b;  
+        int max[20];  
+        int sum = 0,now = 0;  
+        getline(cin,s);  
+  
+        for(int j = 0;j<s.size();j++){  
+            if(s[j]!=' '){  
+                sum++;  
+            }  
+            else{  
+                max[now]= sum;  
+                sum=0;  
+            }  
+        }  
+        sort(max, max+20,cmp);  
+        int maxNum = max[0];  
+        if(i == 0){  
+            cout<<"Tyler Herro Sixth award Miami "<<endl;  
+        }  
+        else{  
+            cout<<"Improved "<<endl;  
+        }  
+    }  
+}  
