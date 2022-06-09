acd13dcd9029ed0bc85f4c19ec7e05397d2576b1
Donkey-duan <jakoo67884520@gmail.com>
2022-06-09 17:04:11 +0800

    master

--- 第二次上機考/4.cpp
+++ 第二次上機考/4.cpp
@@ -1,5 +1,33 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main(){
-    
+    string str;
+    while(cin>>str){
+        int triple = 0,forth = 0;
+        int ansT=0,ansF=0;
+        for(int i = 0;i<str.size();i++){
+            triple+=str[i]-'0';
+        }
+        if(triple%3==0){
+            ansT = 1;
+        }
+        int n = str.size();
+        forth+=(str[n-1]-'0')+(str[n-2]-'0')*10;
+        if(forth%4==0){
+            ansF = 1;
+        }
+        if(ansT && ansF){
+            cout<<str<<" is a multiple of 12."<<endl;
+        }
+        else if(ansT){
+            cout<<str<<" is a multiple of 3."<<endl;
+        }
+        else if(ansF){
+            cout<<str<<" is a multiple of 4."<<endl;
+        }
+        else if((ansT == 0)&&(ansF == 0)){
+            cout<<str<<" is not a multiple of 3 or 4."<<endl;
+        }
+    }    
 }
