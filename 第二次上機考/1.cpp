a66e3f3fdddf254068bd034adfa67160667b42ad
Donkey-duan <jakoo67884520@gmail.com>
2022-06-09 17:24:06 +0800

    master

--- 第二次上機考/1.cpp
+++ 第二次上機考/1.cpp
@@ -3,14 +3,8 @@ using namespace std;
 int main(){
     int n,m;
     while(cin>>n){
-        if(n<1||n>1000){
-            break;
-        }
         int counter = 0;
         cin>>m;
-         if(m<1||m>1000){
-            break;
-        }
         int flag = n;
         for(int i = 1;;i++){
             if(flag>=m){
