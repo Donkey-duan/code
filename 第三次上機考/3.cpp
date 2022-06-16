2e483ebab2594d0901b145cfa0e9fa48d1fa3058
Donkey-duan <jakoo67884520@gmail.com>
2022-06-16 16:50:55 +0800

    master

--- 第三次上機考/3.cpp
+++ 第三次上機考/3.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main(){
-    
+    int n,count = 1,sum = 0;
+    int test = 1;
+    string str;
+    cin>>n>>ws;
+    cin>>str>>ws;
+    for(int i = 0;i<str.size();i++){
+        if(str[i]-'0'==n){
+            count*=n;
+        }
+        else{
+                if(count!=1){
+                    sum+=count;   
+                }
+                count = 1;
+        }
+    }
+    cout<<sum+n<<endl;
 }
